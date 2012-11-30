54a0cc8fce735fe3a57b1376054d5dffb0e2cad3
Matt Thomas <matt@betweenbrain.com>
2012-11-29 23:10:48 -0500

    Fully functional POC with bad word and banned user filtering

--- index.php
+++ index.php
@@ -32,93 +32,82 @@
         ));
 
         $json = curl_exec($curl);
-        // true for associateive array
+        // true for associative array
         $results = json_decode($json, $array);
 
         return $results;
     }
 
-
     $url = 'http://search.twitter.com/search.json?q=';
-    $url .= '%23GuggUBSMAP%20';
+    $url .= '%23joomla';
     $url .= '&result_type=mixed';
     $url .= '&include_entities=1';
+    $url .= '&rpp=100';
 
     $results = searchTwitter($url);
 
-    /*
-    foreach ($results->results as $result) {
-        echo $result->text;
-    }
-    */
-
     foreach ($results['results'] as $result) {
+        // Clear the bad flag
+        $badflag = NULL;
 
-        /*
-        echo '<pre>';
-        echo print_r($result['text']);
-        echo '</pre>';
-        */
-
-        foreach ($result['entities']['hashtags'] as $hashtag) {
-            $url            = 'http://search.twitter.com/search?q=';
-            $obj            = '#' . $hashtag['text'];
-            $replacement    = '<a href="' . $url . $obj . '" >' . $obj . '</a>';
-            $result['text'] = preg_replace("/$obj/i", $replacement, $result['text']);
-
-            /*
-            echo '<pre>';
-            echo 'Hashtag: ' . $hashtag['text'];
-            echo '</pre>';
-            */
-
+        // Check Tweet text for bad words
+        $badwords = array('TopTag', 'Fundraising');
+        foreach ($badwords as $badword) {
+            if (preg_match("/\b$badword\b/i", $result['text'])) {
+                $badflag = TRUE;
+            }
         }
 
-        foreach ($result['entities']['user_mentions'] as $mention) {
-            $url            = 'http://twitter.com/';
-            $obj            = '@' . $mention['screen_name'];
-            $replacement    = '<a href="' . $url . $obj . '" >' . $obj . '</a>';
-            $result['text'] = preg_replace("/$obj/i", $replacement, $result['text']);
-
-            /*
-            echo '<pre>';
-            echo 'Mention: ' . $mention['screen_name'];
-            echo '</pre>';
-            */
-
+        // Check Tweetee for being banned or not
+        $bannedusers = array('betweenbrain', 'suportejoomlabr');
+        foreach ($bannedusers as $banneduser) {
+            if (preg_match("/\b$banneduser\b/i", $result['from_user'])) {
+                $badflag = TRUE;
+            }
         }
 
-        foreach ($result['entities']['urls'] as $urls) {
-            $url            = NULL;
-            $obj            = str_replace(array('/'), array('\/'), $urls['url']);
-            $replacement    = '<a href="' . $url . $obj . '" >' . $obj . '</a>';
-            $result['text'] = preg_replace("/$obj/i", $replacement, $result['text']);
+        // If all is happy in the land of Oz, proceed.
+        if (!$badflag) {
 
-            /*
-            echo '<pre>';
-            echo 'Urls: ' . $urls['url'];
-            echo '</pre>';
-            */
+            // Link hashtags
+            foreach ($result['entities']['hashtags'] as $hashtag) {
+                $url            = 'http://search.twitter.com/search?q=';
+                $obj            = '#' . $hashtag['text'];
+                $replacement    = '<a href="' . $url . $obj . '" >' . $obj . '</a>';
+                $result['text'] = preg_replace("/$obj/i", $replacement, $result['text']);
+            }
 
+            // Link mentions
+            foreach ($result['entities']['user_mentions'] as $mention) {
+                $url            = 'http://twitter.com/';
+                $obj            = '@' . $mention['screen_name'];
+                $replacement    = '<a href="' . $url . $obj . '" >' . $obj . '</a>';
+                $result['text'] = preg_replace("/$obj/i", $replacement, $result['text']);
+            }
+
+            // Link URLs
+            foreach ($result['entities']['urls'] as $urls) {
+                $url            = NULL;
+                $obj            = str_replace(array('/'), array('\/'), $urls['url']);
+                $replacement    = '<a href="' . $url . $obj . '" >' . $urls['url'] . '</a>';
+                $result['text'] = preg_replace("/$obj/i", $replacement, $result['text']);
+            }
+            // Render each Tweet
+            ?>
+        <div style="border: 1px solid #999; margin: 25px;">
+            <img src="<?php echo $result['profile_image_url'] ?>" />
+            <a href="https://twitter.com/<?php echo $result['from_user'] ?>"><?php echo $result['from_user_name'] ?></a>
+            <p>@<?php echo $result['from_user'] ?></p>
+            <p><?php echo $result['text'] ?></p>
+            <p><?php echo  substr($result['created_at'], 4, 7); ?></p>
+            <a href="https://twitter.com/intent/tweet?in_reply_to=<?php echo $result['id'] ?>">Reply</a><br />
+            <a href="https://twitter.com/intent/retweet?tweet_id=<?php echo $result['id'] ?>&via=betweenbrain">Retweet</a><br />
+            <a href="https://twitter.com/intent/favorite?tweet_id=<?php echo $result['id'] ?>">Favorite</a><br />
+        </div>
+            <?php
         }
-
-        ?>
-
-    <div style="border: 1px solid #999; margin: 25px;">
-        <img src="<?php echo $result['profile_image_url'] ?>" />
-        <a href="https://twitter.com/<?php echo $result['from_user'] ?>"><?php echo $result['from_user_name'] ?></a>
-        <p>@<?php echo $result['from_user'] ?></p>
-        <p><?php echo $result['text'] ?></p>
-        <p><?php echo  substr($result['created_at'], 4, 7); ?></p>
-        <a href="https://twitter.com/intent/tweet?in_reply_to=<?php echo $result['id'] ?>">Reply</a><br />
-        <a href="https://twitter.com/intent/retweet?tweet_id=<?php echo $result['id'] ?>&via=betweenbrain">Retweet</a><br />
-        <a href="https://twitter.com/intent/favorite?tweet_id=<?php echo $result['id'] ?>">Favorite</a><br />
-    </div>
-
-        <?php
     }
 ?>
-
 </body>
 </html>
 
