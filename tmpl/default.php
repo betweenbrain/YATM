0b82adcb6d6b3d919fd4f55ca4273a8f634b9891
Matt Thomas <matt@betweenbrain.com>
2012-12-05 16:24:30 -0500

    modify template

--- tmpl/default.php
+++ tmpl/default.php
@@ -10,42 +10,51 @@
 <div class="yatm<?php echo $params->get('moduleclass_sfx'); ?>">
 	<ul>
         <?php foreach ($results->results as $result) {
-            // Filter unwanted Tweets
-            $badflag = $tweet->filterTweet($result, $params);
-            // Load only non-filtered tweets
-            if (!$badflag) {
-                // Link Tweet entities
-                $tweet->linkEntities($result);
-                ?>
-                <li>
-                    <a class="from-user" href="https://twitter.com/<?php echo $result->from_user ?>">
-                        <img class="profile-image" src="<?php echo $result->profile_image_url ?>" />
-                    </a>
-                    <a class="from-user" href="https://twitter.com/<?php echo $result->from_user ?>">
+        // Filter unwanted Tweets
+        $badflag = $tweet->filterTweet($result, $params);
+        // Load only non-filtered tweets
+        if (!$badflag) {
+            // Link Tweet entities
+            $tweet->linkEntities($result);
+            ?>
+			<li class="tweet">
+				<div class="user">
+					<a class="profile-image" href="https://twitter.com/<?php echo $result->from_user ?>">
+						<img class="profile-image" src="<?php echo $result->profile_image_url ?>" />
+					</a>
+					<a class="from-user" href="https://twitter.com/<?php echo $result->from_user ?>">
                         <?php echo $result->from_user_name ?>
-                    </a>
-                    <p class="at-user">
-                        @<?php echo $result->from_user ?>
-                    </p>
-                    <p class="text">
-                        <?php echo $result->text ?>
-                    </p>
-                    <p class="status">
-                        <a href="https://twitter.com/<?php echo $result->from_user . '/status/' . $result->id ?>"><?php echo  substr($result->created_at, 4, 7); ?></a>
-                    </p>
-                    <a class="reply" href="https://twitter.com/intent/tweet?in_reply_to=<?php echo $result->id ?>">
-                        Reply
-                    </a>
-                    <a class="retweet" href="https://twitter.com/intent/retweet?tweet_id=<?php echo $result->id ?><?php if ($via) {echo '&via=' . $via;} ?>">
-                        Retweet
-                    </a>
-                    <a class="favorite" href="https://twitter.com/intent/favorite?tweet_id=<?php echo $result->id ?>">
-                        Favorite
-                    </a>
-                </li>
-                <?php
-            }
+					</a>
+					<p class="at-user">
+						@<?php echo $result->from_user ?>
+					</p>
+				</div>
+				<p class="text">
+                    <?php echo $result->text ?>
+				</p>
+				<a class="status" href="https://twitter.com/<?php echo $result->from_user . '/status/' . $result->id ?>"><?php echo  substr($result->created_at, 4, 7); ?></a>
+
+				<ul class="actions">
+					<li>
+						<a class="reply" href="https://twitter.com/intent/tweet?in_reply_to=<?php echo $result->id ?>">
+							Reply
+						</a>
+					</li>
+					<li>
+						<a class="retweet" href="https://twitter.com/intent/retweet?tweet_id=<?php echo $result->id ?><?php if ($via) { echo '&via=' . $via; } ?>">
+							Retweet
+						</a>
+					</li>
+					<li>
+						<a class="favorite" href="https://twitter.com/intent/favorite?tweet_id=<?php echo $result->id ?>">
+							Favorite
+						</a>
+					</li>
+				</ul>
+			</li>
+            <?php
         }
+    }
         ?>
 	</ul>
 </div>
